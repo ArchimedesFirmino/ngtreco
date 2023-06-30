41f7577fb391a0e26f2e78f79b021ec643053f68
Archimedes Firmino <archimedessfirmino@gmail.com>
2023-06-29 21:36:12 -0300

    Aplicação do Post na página Contatos.

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,10 +1,43 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent {
+  public env = environment;
+  myForm!: FormGroup;
+  contact: any;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) { }
+
+
+  ngOnInit() {
+    this.createForm();
+  }
+  createForm() {
+    this.myForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    })
+  }
+
+
+  sendContact(){
+    const today = new Date();
+    this.contact = this.myForm.value;
+    this.contact.date = today.toISOString().replace('T', ' ').split('.')[0];
+    this.contact.status = 'received';
+    // Salva na API.
+    this.http.post(`${this.env.apiBaseURL}/contatos`, this.contact)
+
+  }
 }
